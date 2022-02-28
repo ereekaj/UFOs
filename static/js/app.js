cd141f4056820565110712cb628307a65d4392ef
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-02-28 16:16:31 -0700

    built js code for module

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 // import data from data.js
 const tableData = data;
 
-// reference HTML table using DS library
+// reference HTML table using D3 JS library
 var tbody = d3.select("tbody"); 
 
 function buildTable(data) {
@@ -22,4 +22,32 @@ function buildTable(data) {
         }
       );
     });
-  }
+  };
+  
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    
+        // Check to see if a date was entered and filter the
+    // data using that date.
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    
+        // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+    };
+
+// looks for HTML button element executes handleClick fxn when button is clicked
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
