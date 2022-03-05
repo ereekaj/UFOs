fd393a37f18c6de904514683a45cbb9fcf950796
ereekaj <52435827+ereekaj@users.noreply.github.com>
2022-03-05 12:07:20 -0600

    challenge code

--- static/js/app.js
+++ static/js/app.js
@@ -30,11 +30,11 @@ var filters = []
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-
+  var filterChange = d3.select(this);
     // 4b. Save the value that was changed as a variable.
-
+  var filterValue = d3.select(this).select("text");
     // 4c. Save the id of the filter that was changed as a variable.
-
+  var filterId = d3.select(this).select('id');
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
